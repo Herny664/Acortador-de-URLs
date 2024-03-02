059a656f2af7eff861b473c129d57fc8fdcf5ddc
Herny664 <hern4n.roch0n@gmail.com>
2024-03-01 23:11:45 -0300

    Acortador de URLs V1

---
+++ Acortador_URL.py
@@ -0,0 +1,16 @@
+import pyshorteners
+import streamlit as st
+
+def shorten_url(url):
+    s = pyshorteners.Shortener()
+    shortened_url = s.tinyurl.short(url)
+    return shortened_url
+
+#Creación de App Web con Streamlit
+st.set_page_config(page_title="URL Shortener", page_icon="🌐🔗", layout="centered")
+st.image("img/url.png", use_column_width=True)
+st.title("URL Shortener")
+st.header("Simple and efective")
+url = st.text_input("Enter the URL")
+if st.button("Generate new URL"):
+    st.write("URL shortened: ", shorten_url(url))
