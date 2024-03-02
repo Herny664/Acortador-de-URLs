1089b401b75007d8abcdda0bf9c5efdad8c68c59
Herny664 <hern4n.roch0n@gmail.com>
2024-03-02 16:36:31 -0300

    Icono cambiado

--- Acortador_URL.py
+++ Acortador_URL.py
@@ -7,7 +7,7 @@ def shorten_url(url):
     return shortened_url
 
 #Creación de App Web con Streamlit
-st.set_page_config(page_title="URL Shortener", page_icon="🌐🔗", layout="centered")
+st.set_page_config(page_title="URL Shortener", page_icon="🔗", layout="centered")
 st.image("img/url.png", use_column_width=True)
 st.title("URL Shortener")
 st.subheader("Simple and efective")
